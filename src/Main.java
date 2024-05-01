aaea3d1bd57b84acfa52e1d59f59741f545b32a6
player2810 <danielbalut28@gmail.com>
2024-05-01 09:56:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+            Player1 player = new Player1(100, "sword", "Daniel");
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+    player.damageByGun1();
+    player.damageByGun1();
+    player.damageByGun2();
+    player.heal();
+
+    }
+}
